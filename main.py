1abe7044ea2ded8b0adfa9d0da6820f90b642bc4
Ashish <jadhavashish028@gmail.com>
2022-02-27 00:16:16 +0530

    __name__ 27-Feb-2022

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
+print("Always executed")
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+if __name__ == "__main__":
+    from Phone_number_Details import *
+    print("Executed when invoked directly")
+else:
+    print("Executed when imported")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
